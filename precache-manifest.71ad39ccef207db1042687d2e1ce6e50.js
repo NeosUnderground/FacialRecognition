6b077497eae3bf35949388fd8f504dfddb0a1dc9
NeosUnderground <33915360+NeosUnderground@users.noreply.github.com>
2019-06-16 23:04:33 -0500

    Updates

---
+++ precache-manifest.71ad39ccef207db1042687d2e1ce6e50.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef35c01ec62bfefce8eb86ee7edac266",
+    "url": "/FacialRecognition/index.html"
+  },
+  {
+    "revision": "27a7f9abb16918e8d772",
+    "url": "/FacialRecognition/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "b9547a43a4370619b0df",
+    "url": "/FacialRecognition/static/css/main.d1e356b9.chunk.css"
+  },
+  {
+    "revision": "27a7f9abb16918e8d772",
+    "url": "/FacialRecognition/static/js/2.ee2e66d1.chunk.js"
+  },
+  {
+    "revision": "b9547a43a4370619b0df",
+    "url": "/FacialRecognition/static/js/main.dd2707ea.chunk.js"
+  },
+  {
+    "revision": "b1d6580c11f6e5922aa8",
+    "url": "/FacialRecognition/static/js/runtime~main.ab9aff6f.js"
+  },
+  {
+    "revision": "5ffac590a9aa72a449fa066f0d40490b",
+    "url": "/FacialRecognition/static/media/brain.5ffac590.png"
+  }
+]);
